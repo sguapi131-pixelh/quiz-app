75c291c18c5517fa2948411bc706da7dea682442
sguapi131-pixelh <sguapi131@gmail.com>
2026-09-11 10:58:33 +0800

    Cache built-in source question bank

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "quiz-pwa-v2";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "icon-192.png", "icon-512.png"];
+const CACHE = "quiz-pwa-source-docx-v3";
+const ASSETS = ["./", "index.html", "styles.css", "questions-data.js", "app.js", "manifest.json", "icon-192.png", "icon-512.png"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
 self.addEventListener("activate", event => event.waitUntil(
   caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
