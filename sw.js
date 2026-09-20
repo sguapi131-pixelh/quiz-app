8e654464535faa591b974143e5182549473aeb69
sguapi131-pixelh <sguapi131@gmail.com>
2026-09-20 10:30:54 +0800

    Bump PWA cache for resume support

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "quiz-pwa-v10-filtered";
+const CACHE = "quiz-pwa-v11-resume";
 const ASSETS = [
   "./", "index.html", "styles.css", "manifest.json", "icon-192.png", "icon-512.png", "app-v3.js",
   "questions-1a.js", "questions-1b.js", "questions-2.js", "questions-3a.js", "questions-3b.js",
